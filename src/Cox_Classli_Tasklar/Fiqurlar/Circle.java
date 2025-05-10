e987e25873bb2ae29029a0bed53c786ed8bfce47
mayis <mayisqarayev75@gmail,com>
2025-05-10 14:12:35 +0400

    duzelis

--- src/Cox_Classli_Tasklar/Fiqurlar/Circle.java
+++ src/Cox_Classli_Tasklar/Fiqurlar/Circle.java
@@ -5,6 +5,10 @@ public class Circle extends Shape{
     private Integer radius;
     private final static Double PI = 3.14;
 
+    public Circle(Integer radius) {
+        this.radius = radius;
+    }
+
     public Integer calculateArea() {
         return (int)(PI * Math.pow(radius, 2));
     }
